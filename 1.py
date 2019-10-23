df08d61662d74f112f56810787301ecf99d3d19f
sofmeireles <sofiamfc24964@gmail.com>
2019-10-23 16:08:03 +0100

    turtle composto

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-#ola
+from turtle import Screen, Turtle
+
+screen = Screen()
+t = Turtle()
+
+def ola():
+
+    t.pensize(3)
+    t.pencolor('yellow')
+    t.circle(20)
+    t.pu()
+    screen.mainloop()
+
+ola()
